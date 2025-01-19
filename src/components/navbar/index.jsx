ddc9dfd4007d4af4b97c4ef36fe390cceaa5f033
Vijay18110 <vijay9628554907@gmail.com>
2025-01-19 14:37:17 +0530

    sidebar error fixed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './index.module.css';
-import Toggle from '../../pages/toggle/index.jsx';
-import Sidebarnew from '../../pages/sidebarnew/index.jsx';
+import Toggle2 from '../../pages/Toggle/index.jsx';
+import Sidebarnew from '../../pages/Sidebarnew/index.jsx';
 const NavBar = () => {
     const [toggle, setToggle] = React.useState(false);
     return (
         <>
             <div className={styles.cont1}>
-                <Toggle setToggle={setToggle} />
+                <Toggle2 setToggle={setToggle} />
             </div>
             {toggle && <Sidebarnew setToggle={setToggle} />
             }
